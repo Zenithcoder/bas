5687c00b7f665fdc9926019be84693e6fb914fda
Awonusi <awonusiolajide@yahoo.com>
2017-08-02 11:41:29 +0100

    first commit

---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,8 @@
+<div class="jumbotron text-center">
+	<div class="container">
+		<h1> Welcome to Our Sites</h1>
+		<p class="lead">Welcine to our brand new Website created in Laravel 5.4</p>
+		
+	</div>
+	
+</div>
